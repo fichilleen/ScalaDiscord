1354cdc545a4134001483b41f6bf8a7136b7b694
Pauric <paurics@gmail.com>
2021-01-03 21:16:59 +0000

    Minor reformatting

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -13,8 +13,8 @@ object Main extends App {
   implicit val as: ActorSystem = ActorSystem("tests")
 
   def textChannelToGuildChannel(textChannelId: TextChannelId)(implicit c: CacheSnapshot): Option[GuildChannel] = {
-     textChannelId.asChannelId[GuildChannel].resolve
-    }
+    textChannelId.asChannelId[GuildChannel].resolve
+  }
 
   val config = Config.config match {
     case Right(value) => value
@@ -22,25 +22,28 @@ object Main extends App {
   }
 
   val clientSettings = ClientSettings(config.discordToken)
-  //The client settings contains an excecution context that you can use before you have access to the client
-  //import clientSettings.executionContext
+  println(config.discordToken)
 
-  val client = Await.result(clientSettings.createClient(), Duration.Inf)
   import client.executionContext
+  val client = Await.result(clientSettings.createClient(), Duration.Inf)
+
   client.login()
+  println("logged in")
 
   val fetchFilmInfo = new FetchFilmInfo(config.omdbapiToken)
 
-  // This doesn't work, not sure why
   val cmd = new FilmCommand(client.requests)
   client.commands.runNewNamedCommand(cmd.hello)
 
-  client.onEventAsync { implicit cacheSnapshot => {
-    case APIMessage.MessageCreate(guild, message, _) =>
 
+  client.onEventAsync { implicit cacheSnapshot => {
+    case APIMessage.Ready(_) =>
+      println("Connected")
+      OptFuture.fromOption(None)
+
+    case APIMessage.MessageCreate(guild, message, _) =>
       println(s"On guild $guild")
       println(s" channel.to_str = '${message.channelId}''")
-
 
       if (message.content.toLowerCase.startsWith("fuck you") && message.authorUsername != clientName) {
         val msg = CreateMessage(message.channelId, CreateMessageData(content = s"Fuck you ${message.authorUsername}"))
@@ -48,33 +51,27 @@ object Main extends App {
       }
 
       else if (guild.isDefined) {
-          val chan = textChannelToGuildChannel(message.channelId)
-          chan.foreach{ c =>
-            println("got a defined channel")
-            println(s"called ${c.name}")
-          }
-          if ((chan.isDefined) && (chan.get.name == "saturday-film-votes") && (message.authorUsername != clientName)) {
-            println("in saturday film votes")
-            OptFuture.fromFuture(
-              fetchFilmInfo.fetchByTitle(message.content).map { f =>
-                client.requestsHelper.run(CreateMessage(message.channelId, CreateMessageData(content = f.asMessage))).map(
-                  println
-                )
-              }
-            )
-          }
-          else OptFuture.fromOption(None)
+        val chan = textChannelToGuildChannel(message.channelId)
+        chan.foreach { c =>
+          println("got a defined channel")
+          println(s"called ${c.name}")
         }
+        if ((chan.isDefined) && (chan.get.name == "saturday-film-votes") && (message.authorUsername != clientName)) {
+          println("in saturday film votes")
+          OptFuture.fromFuture(
+            fetchFilmInfo.fetchByTitle(message.content).map { f =>
+              client.requestsHelper.run(CreateMessage(message.channelId, CreateMessageData(content = f.asMessage))).map(
+                println
+              )
+            }
+          )
+        }
+        else OptFuture.fromOption(None)
+      }
       else {
         OptFuture.fromOption(None)
       }
-  }}
-
-  client.onEventSideEffectsIgnore {
-    case APIMessage.Ready(_) =>
-      println("Connected")
-    case x =>
-      println(s"other - $x")
+    }
   }
-
+    println("somehow got to the end")
 }
